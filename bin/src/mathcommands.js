1f95a678bf507dcb5defe429749b56833f6e1cb5
eggsnham07 <eggsnham07@gmail.com>
2021-10-28 18:26:15 +0000

    Fixed typescript issues

--- bin/src/mathcommands.js
+++ bin/src/mathcommands.js
@@ -1,19 +1,19 @@
 #!/usr/bin/env node
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-var commander_1 = require("commander");
-var package_json_1 = require("../package.json");
-var program = new commander_1.Command();
+const commander_1 = require("commander");
+const package_json_1 = require("../package.json");
+const program = new commander_1.Command();
 program
     .option("-a --add <number>", "Adds numbers")
     .option("-m --multiply <numbers>", "Multiplys numbers")
     .option("-h --help", "Gets help")
     .option("-d --debug", "Enables debugging", false);
 program.parse(process.argv);
-program.name();
+program.name(String(package_json_1.name).split("/")[2]);
 program.showSuggestionAfterError();
 program.showHelpAfterError();
-var args = program.opts();
+const args = program.opts();
 var debug = args["debug"];
 if (debug) {
     console.log(program.opts());
@@ -23,9 +23,9 @@ if (args["help"]) {
     program.outputHelp();
 }
 if (args["add"]) {
-    var nums = args["add"].split(" ");
+    const nums = args["add"].split(" ");
     var result = 0;
-    nums.forEach(function (num) {
+    nums.forEach((num) => {
         result += Number(num);
     });
     console.log(Number(result));
--- mathcommands.ts
+++ mathcommands.ts
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 import { Command } from "commander"
-import { name } from "../package.json"
+import { version, name } from "./package.json"
 
 const program = new Command()
 program
@@ -8,19 +8,17 @@ program
     .option("-m --multiply <numbers>", "Multiplys numbers")
     .option("-h --help", "Gets help")
     .option("-d --debug", "Enables debugging", false)
+    .option("-v --version", "Shows current version")
 
 program.parse(process.argv)
-program.name()
+program.name(String(name).split("/")[2])
 program.showSuggestionAfterError()
 program.showHelpAfterError()
 
 const args = program.opts()
 var debug = args["debug"]
 
-if(debug) {
-    console.log(program.opts())
-    console.log(name)
-}
+if(args["version"]) console.log(`V${version}`)
 
 if(args["help"]) {
     program.outputHelp()
@@ -35,4 +33,8 @@ if(args["add"]) {
     })
 
     console.log(Number(result))
+}
+
+if(debug) {
+    console.log("Debug section:\n\n", program.opts())
 }
